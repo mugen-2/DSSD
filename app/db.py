bd608d4f5b41460ad57ce874fa56ad8bf2296257
mugen-2 <kaius1833@gmail.com>
2022-09-28 13:06:10 -0300

    Cambios 2

--- app/db.py
+++ app/db.py
@@ -5,14 +5,7 @@ db = SQLAlchemy()
 def init_app(app):
     db.init_app(app)
     with app.app_context():
-        from app.models.rol import Rol
-        from app.models.permiso import Permiso
         from app.models.user import User
-        from app.models.punto_encuentro import PuntoEncuentro
-        from app.models.config import Config
-        from app.models.zonas_inundables import ZonasInundables
-        from app.models.seguimiento import Seguimiento
-        from app.models.punto_encuentro import PuntoEncuentro
         config_db(app)
 
 def config_db(app):
---
+++ app/models/user.py
@@ -0,0 +1,34 @@
+from typing import List
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, update
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.mysql import TINYINT
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin
+from app.db import db
+
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True)
+    first_name = Column(String(100))
+    last_name = Column(String(100))
+    email = Column(String(100), unique=True)
+    password = Column(String(300))
+    username = Column(String(100), unique=True)
+    area = Column(String(300))
+    #google=Column(TINYINT)
+
+def __init__(self, first_name=None, last_name=None, email=None, password=None, username=None, area=None, google=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.username = username
+        self.area = area 
+        #self.google=google
+        
+  
+
+
+
